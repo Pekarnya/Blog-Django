85c0987ae6fadf83feac98d73263f07a97932828
Grigorij Tckachencko <tckachencko.grigorij@yandex.ru>
2023-04-06 02:07:49 +0300

    Added user profile Model, Minor fixes

--- test_blog/blog/models.py
+++ test_blog/blog/models.py
@@ -2,11 +2,16 @@
 Returns:
     _type_: _description_
 """
-from django.db import models
-from django.contrib.auth.models import User
+import uuid
 from django.utils import timezone
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+user = get_user_model()
 
 
 class Post(models.Model):
@@ -18,7 +23,26 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_posted = models.DateField(default=timezone.now())
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Profile(models.Model):
+    """Profile that represents a user profile class."""
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+    id_user = models.BigIntegerField(null=True)
+    unique_id = models.UUIDField(null=False, default=uuid.uuid4)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_img',
+                                    default='basic_profile.jpg')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self) -> str:
+        """Username method
+
+        Returns:
+            str: Username info
+        """
+        return self.user.username
