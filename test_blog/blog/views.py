2e38f8dc51d81cfae7d93bab5cd86f9891c66e69
Grigorij Tckachencko <tckachencko.grigorij@yandex.ru>
2023-04-06 02:01:21 +0300

    Added signup functionality

--- test_blog/blog/views.py
+++ test_blog/blog/views.py
@@ -3,33 +3,63 @@ Returns:
     _type_: DOM object
 """
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
-from .forms import NewUserForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Profile
+
 # Create your views here.
 
 POST = [
-    {'author': 'Anonymous',
-    'title': 'Mildsēoc Murka!',
-    'content': 'Frēondlīc Murka is here to assisteth thee',
-    'date' : '2023',
-    },
+    {'author': 'Anonymous', 'title': 'Mildsēoc Murka!',
+     'content': 'Frēondlīc Murka is here to assisteth thee',
+     'date': '2023'},
 ]
 
 
 def register_page(request):
+    """Create a new profile from POST request
+    posting unique identifier
+
+    Args:
+        request (POST): POST request from submit form
+
+    Returns:
+        User.objects.create_user: add user data to the database
+        Profile.objects.create: Creates new profile
+    """
 
     if request.method == 'POST':
-        form = NewUserForm(request.POST)
-        user = form.save()
-        login(request, user)
-        messages.success(request, 'You`re welcome!')
-        return redirect('')
-        # messages.error(request, 'You gave us wrong info don`t you?')
-        # form.save()
-    
-    form = NewUserForm()
-    return render(request=request, template_name='blog/login.html')
+        username = request.POST['username']
+        email = request.POST['email']
+        pass1 = request.POST['password1']
+        pass2 = request.POST['password2']
+        if pass1 == pass2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('blog-register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('blog-register')
+            else:
+                messages.info(request, message=f"Welcome {username}")
+                user = User.objects.create_user(username=username,
+                                                email=email, password=pass1)
+                user.save()
+
+                # login user and redirect
+
+                # create Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,
+                                                     id=user_model.id)
+                new_profile.save()
+                return redirect('blog-login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('blog-register')
+
+    else:
+        return render(request, template_name='blog/login.html')
 
 
 def home(request):
@@ -44,7 +74,7 @@ def home(request):
     context = {
         'posts': POST
     }
-    return render(request, 'blog/home.html', context) 
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
@@ -89,9 +119,9 @@ def godown(request):
 
 def contacts(request):
     """Render a contact page
-    
+
     Args: request (Any): get request
-    
+
     returns:
         _type_: contacts.html
     """
